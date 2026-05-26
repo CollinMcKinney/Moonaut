cf54af6c8cf60926d48aed165163e0d444d639b6
Collin McKinney <collinmckinney4@gmail.com>
2026-05-26 00:35:03 -0500

    Change default window size to (16:9 * 16 * 4) from (16:9 * 16 *5).

--- main.c
+++ main.c
@@ -70,7 +70,7 @@ static void print_fps(double now)
    ====================================================================== */
 int main(void)
 {
-    const int window_size = 5;
+    const int window_size = 4;
     const int width  = 256 * window_size;
     const int height = 144 * window_size;
     const double max_frame_time = 0.25;
